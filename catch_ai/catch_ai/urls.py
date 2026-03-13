166d010680e7e68f20016686a5d3355bae9eae11
logiflydevelopers-cloud <logiflydevelopers@gmail.com>
2026-03-13 10:07:36 +0530

    Updated requirements.txt

--- catch_ai/catch_ai/urls.py
+++ catch_ai/catch_ai/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+def home(request):
+    return JsonResponse({"status": "API running"})
 
 urlpatterns = [
+    path("", home),
     path('admin/', admin.site.urls),
     path('api/users/', include("apps.users.urls")),
 ]
