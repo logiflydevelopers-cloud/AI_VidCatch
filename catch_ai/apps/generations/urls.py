25ed1a7f39ef0350ee3730ca39d8832dd672a92a
logiflydevelopers-cloud <logiflydevelopers@gmail.com>
2026-03-17 15:02:03 +0530

    Generations set-up

--- catch_ai/apps/generations/urls.py
+++ catch_ai/apps/generations/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path("create/", views.create_generation, name="create_generation"),
 
     # get generation status
-    path("<str:job_id>/", views.get_generation, name="get_generation"),
+#     path("<str:job_id>/", views.get_generation, name="get_generation"),
 
-    # list user generations
-    path("my/", views.list_generations, name="my_generations"),
-]
+#     # list user generations
+#     path("my/", views.list_generations, name="my_generations"),
+# ]
