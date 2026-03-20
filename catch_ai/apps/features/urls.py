5fc32b86fb733aa34689981305ad024d4873694b
logiflydevelopers-cloud <logiflydevelopers@gmail.com>
2026-03-20 17:52:09 +0530

    Features Updated

--- catch_ai/apps/features/urls.py
+++ catch_ai/apps/features/urls.py
@@ -3,6 +3,6 @@ from .views import list_features, get_feature
 
 
 urlpatterns = [
-    path("features/", list_features, name="list-features"),
-    path("features/<str:feature_id>/", get_feature, name="get-feature"),
+    path("", list_features, name="list-features"),
+    path("<str:feature_id>/", get_feature, name="get-feature"),
 ]
