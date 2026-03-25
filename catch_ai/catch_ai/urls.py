960491f5e2d52dcf33873bd65b2549992b682fee
logiflydevelopers-cloud <logiflydevelopers@gmail.com>
2026-03-25 09:54:18 +0530

    Template Admin Updated

--- catch_ai/catch_ai/urls.py
+++ catch_ai/catch_ai/urls.py
@@ -20,11 +20,18 @@ from django.http import JsonResponse
 from django.conf import settings
 from django.conf.urls.static import static
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 def home(request):
     return JsonResponse({"status": "API running"})
 
 urlpatterns = [
     path("", home),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('admin/', admin.site.urls),
     path('api/users/', include("apps.users.urls")),
     path("api/templates/", include("apps.templates.urls")),
